123ea166d1b31b0df3dbaf33f3adf2073fbddc2b
Felipe Almeida <alm.felipe@gmail.com>
2021-03-11 16:40:07 -0300

    import vuejs-loading-screen

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import loading from 'vuejs-loading-screen'
+ 
+Vue.use(loading)
 Vue.config.productionTip = false
 
 new Vue({
