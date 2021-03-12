feaa77a489730f1c67e64f4e0c71919f8c3df978
Felipe Almeida <alm.felipe@gmail.com>
2021-03-12 00:36:39 -0300

    set about text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <h3>vue-weather-forecast-brazil</h3>
+    <div class="text-justify mt-4">
+      Vue-weather-forecast-brazil consults APIs from INMET (National Institute of Meteorology) and IBGE (Brazilian Institute of Geography and Statistics) to provide weather forecasts for Brazilian cities. Go to <a href="https://github.com/almfelipe/vue-weather-forecast-brazil"> github project page</a> for more information.
+    </div>
   </div>
 </template>
